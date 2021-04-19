4ced5d6291e662ebc9275ad02557c21399b7789d
Eze Ochonogor <sendezeamail@gmail.com>
2021-04-19 16:06:26 +0100

    V3

--- src/components/ribbons.js
+++ src/components/ribbons.js
@@ -5,7 +5,7 @@ const Ribbons = () => {
      return (
           <div className='grid grid-cols-2 gap-1  w-11/12 my-2 mx-auto'>
                <Ribbon name="AFYA" number="143.00" text="Balance" ></Ribbon>
-               <Ribbon name="ZAR" number="R21.00" text="Equivalent" ></Ribbon>
+               <Ribbon name="ZAR" direction='right' number="R21.00" text="Equivalent" ></Ribbon>
           </div>
      )
 }
